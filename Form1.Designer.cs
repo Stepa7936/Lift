3ee294ee1d49b2b53608916bb01a16f18a328304
Stepa7936 <76559787+Stepa7936@users.noreply.github.com>
2023-09-03 13:38:47 +0300

    Лифт
    В данном приложении, небольшая работа лифта, при нажатии на кнопки лифт перемещается на определенный этаж. Также учтено, что если лифт едет на 1 этаж, он не поедет на 3, если находится на втором.

---
+++ Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿
+namespace lift
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel5.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(19, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(57, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "3";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(36, 212);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(97, 189);
+            this.panel1.TabIndex = 1;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(19, 129);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(57, 47);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "1";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(19, 76);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(57, 47);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel2.Controls.Add(this.pictureBox4);
+            this.panel2.Controls.Add(this.pictureBox3);
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Controls.Add(this.panel5);
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Location = new System.Drawing.Point(292, 29);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(442, 372);
+            this.panel2.TabIndex = 2;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(24, 47);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(59, 50);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(24, 176);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(59, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(24, 309);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(59, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.SystemColors.Info;
+            this.panel5.Controls.Add(this.label3);
+            this.panel5.Location = new System.Drawing.Point(199, 31);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(225, 66);
+            this.panel5.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(77, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 39);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "3 этаж";
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.Info;
+            this.panel4.Controls.Add(this.label2);
+            this.panel4.Location = new System.Drawing.Point(199, 160);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(225, 66);
+            this.panel4.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(77, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 39);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "2 этаж";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.Info;
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Location = new System.Drawing.Point(199, 293);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(225, 66);
+            this.panel3.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(77, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1 этаж";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(105, 292);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(66, 66);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lift
+{
+    public partial class Form1 : Form
+    {
+        int k = 1; int j = 0; bool r;
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Location = new Point(80, 240);
+            timer1.Stop();
+            timer2.Stop();
+            pictureBox2.Visible = false;
+            pictureBox3.Visible = false;
+            pictureBox4.Visible = false;
+            r = false;
+        }
+        public void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!r)
+            {
+
+
+                if (button3.ForeColor == Color.Red&& button2.ForeColor == Color.Black)
+                {
+                    pictureBox3.Visible = false;
+                    pictureBox4.Visible = false;
+                    timer1.Start();
+                    pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 3);
+                    if (pictureBox1.Location.Y >= 240)
+                    {
+                        button3.ForeColor = Color.Black;
+                        timer1.Stop();
+                        k = 1;
+                        pictureBox2.Visible = true;
+                        r = true;
+                        timer2.Start();
+                    }
+                }
+                else if (k == 1 && button2.ForeColor == Color.Red && button1.ForeColor != Color.Red)
+                {
+                    pictureBox2.Visible = false;
+                    pictureBox4.Visible = false;
+                    timer1.Start();
+                    pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 3);
+                    if (pictureBox1.Location.Y <= 130)
+                    {
+                        button2.ForeColor = Color.Black;
+                        timer1.Stop();
+                        k = 2;
+                        pictureBox3.Visible = true;
+                        r = true;
+                        timer2.Start();
+                    }
+                }
+
+                else if (k == 3 && button2.ForeColor == Color.Red)
+                {
+                    pictureBox2.Visible = false;
+                    pictureBox4.Visible = false;
+                    timer1.Start();
+                    pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 3);
+                    if (pictureBox1.Location.Y >= 130)
+                    {
+                        button2.ForeColor = Color.Black;
+                        timer1.Stop();
+                        k = 2;
+                        pictureBox3.Visible = true;
+                        r = true;
+                        timer2.Start();
+
+                    }
+                }
+                else if (button1.ForeColor == Color.Red && k != 3)
+                {
+
+
+                    pictureBox2.Visible = false;
+                    pictureBox3.Visible = false;
+                    timer1.Start();
+                    pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 3);
+                    if (pictureBox1.Location.Y <= 26)
+                    {
+                        button1.ForeColor = Color.Black;
+                        timer1.Stop();
+                        k = 3;
+                        pictureBox4.Visible = true;
+                        r = true;
+                        timer2.Start();
+                    }
+                }
+            }
+        }
+    
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.ForeColor = Color.Red;
+            timer1.Start();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.ForeColor = Color.Red;
+            timer1.Start();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.ForeColor = Color.Red;
+            timer1.Start();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (r)
+            {
+                j++;
+                if (j == 50)
+                {
+                    r = false;
+                    pictureBox3.Visible = false;
+                    pictureBox2.Visible = false;
+                    pictureBox4.Visible = false;
+                    timer2.Stop();
+                    j = 0;
+                    timer1.Start();
+                }
+            }
+        }
+    }
+}
